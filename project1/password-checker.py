cdc5d9b8cb038f3b68bee79cc758d1da68a3719b
Armita <farshinarmita@gmail.com>
2025-07-10 22:53:56 +0330

    Update password-checker.py

--- project1/password-checker.py
+++ project1/password-checker.py
@@ -1,14 +1,17 @@
 import re
+import random
 user_pass = input("Please enter your password...")
 num = len(user_pass)
+length = random.randint(7, 10)
+extra = ''.join(random.choice(user_pass) for _ in range(length))
 if (re.search(r'[A-Z]', user_pass) and
     re.search(r'[a-z]', user_pass) and
     re.search(r'[0-9]', user_pass) and
     re.search(r'[!@#$%&]')):
       if num < 4:
         print("Too short!")
-      elif 4 < num < 6: #now i should suggest something for renforcement, we can suggest randomly from the main pass
-        print("Weak")
+      elif 4 < num < 6: 
+        print(f"Weak, how about {extra} ?")
       elif 6 < num < 8:
         print("Medium")
       elif 8 < num < 13:
