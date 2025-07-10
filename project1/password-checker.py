117c24c14a8e1a2d9bde54923a67d7a6698937a4
Armita <farshinarmita@gmail.com>
2025-07-10 23:00:00 +0330

    Update password-checker.py

--- project1/password-checker.py
+++ project1/password-checker.py
@@ -3,7 +3,8 @@ import random
 user_pass = input("Please enter your password...")
 num = len(user_pass)
 length = random.randint(7, 10)
-extra = ''.join(random.choice(user_pass) for _ in range(length))
+extra1 = ''.join(random.choice(user_pass) for _ in range(length))
+extra2 = ''.join(random.choice(user_pass) for _ in range(length))
 if (re.search(r'[A-Z]', user_pass) and
     re.search(r'[a-z]', user_pass) and
     re.search(r'[0-9]', user_pass) and
@@ -11,7 +12,7 @@ if (re.search(r'[A-Z]', user_pass) and
       if num < 4:
         print("Too short!")
       elif 4 < num < 6: 
-        print(f"Weak, how about {extra} ?")
+        print(f"Weak, how about {extra1} or {extra2} ?") 
       elif 6 < num < 8:
         print("Medium")
       elif 8 < num < 13:
